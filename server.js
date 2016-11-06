fe01726e20eda6274692fe5349cc4f3b26e0406f
Gagandeep Singh <gagandeep.singh.iitdelhi@gmail.com>
2016-11-06 18:23:12 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// console.out('Server started');
+
+app.get('/', function(req, res) {
+	res.send('Welcome to the home page');
+});
+
+app.get('/about', function(req, res) {
+	res.send('This was implemented by GSK');
+});
+
+app.listen(3000);
